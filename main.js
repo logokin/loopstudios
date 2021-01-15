0d2d06a4c5de672d83cce95245d15ea4de7be229
obika chibuike <obikachibuike29@gmail.com>
2021-01-15 12:33:45 +0100

    completed the home page

--- main.js
+++ main.js
@@ -10,3 +10,11 @@ links.addEventListener("click", () => {
   links.classList.remove("header-links-show");
   document.querySelector("body").style.overflow = "scroll";
 });
+
+const date = document.querySelector(".year");
+
+const year = new Date();
+const getYear = year.getFullYear();
+console.log(getYear);
+
+date.innerHTML = getYear;
